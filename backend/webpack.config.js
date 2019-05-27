eb8a4ba52a8183bc559b12d5f38eb8502f7ec3d7
David Williams <thedavesignal@gmail.com>
2019-05-27 15:34:56 -0700

    Backend now serves React frontend. Frontend development continues in own branch.

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, 'build')
   },
   target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
   externals: [nodeExternals()],
   module: {
     rules: [
