116d477bb07f4f9a43b513d21526d984fd702292
David Williams <thedavesignal@gmail.com>
2019-06-17 21:50:54 -0700

    Production build compiled for deployment

---
+++ frontend/build/precache-manifest.a7f155c024e3a5980482599b16cd7905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0699e0327f30381b6866a60672650794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74aa0e0fae1dd00c9a82",
+    "url": "/static/css/main.d9bb1a4d.chunk.css"
+  },
+  {
+    "revision": "6d14d2709d196891e6be",
+    "url": "/static/js/2.91205bc6.chunk.js"
+  },
+  {
+    "revision": "74aa0e0fae1dd00c9a82",
+    "url": "/static/js/main.f3754244.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
