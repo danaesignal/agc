9e4288b29001b6911f9ee72bbe47a0df881ebee3
David Williams <thedavesignal@gmail.com>
2019-07-10 11:34:37 -0700

    Code cleanup: removed outdated comment

--- backend/src/auction/auction.model.js
+++ backend/src/auction/auction.model.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 let auctionSchema = new Schema({
   server: {type: String, required: true},
   items: {type: String, required: true}
-  // buyout: {type: Number, required: true}
 });
 
 export default mongoose.model('Auction', auctionSchema);
